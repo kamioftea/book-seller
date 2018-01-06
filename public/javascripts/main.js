22f8e0cbe44df247903a64125860171cc3887f54
Jeff Horton <jeff@goblinoid.co.uk>
2018-01-06 04:49:28 +0000

    basic barcode search box

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,34 +1,49 @@
 $(document).foundation();
 
-var keyBuffer = "";
-var timeoutID = null;
+$(function () {
 
-$(document).keypress(function (event) {
-    if ((event.which === 10 || event.which === 13) && keyBuffer.length > 3) {
-        event.preventDefault();
-        location.pathname = "/barcode/" + keyBuffer;
+    var keyBuffer = "";
+    var timeoutID = null;
+    var input = $('#barcode-input');
+
+    $(document).keypress(function (event) {
+        if ((event.which === 10 || event.which === 13) && keyBuffer.length > 3) {
+            event.preventDefault();
+            location.pathname = "/barcode/" + keyBuffer;
+            keyBuffer = "";
+            if(timeout)
+            {
+                clearTimeout(timeoutID);
+            }
+            return;
+        }
+
+        var char = String.fromCharCode(event.which);
+        if(char.match(/[0-9]/))
+        {
+            event.preventDefault();
+            keyBuffer += char;
+            if(timeoutID)
+            {
+                clearTimeout(timeoutID)
+            }
+            timeoutID = setTimeout(clearBuffer, 100)
+        }
+    });
+
+    function clearBuffer() {
+        input.val(input.val() + keyBuffer);
         keyBuffer = "";
-        if(timeout)
-        {
-            clearTimeout(timeoutID);
-        }
-        return;
+        timeoutID = null;
     }
 
-    var char = String.fromCharCode(event.which);
-    if(char.match(/[0-9]/))
-    {
-        event.preventDefault();
-        keyBuffer += char;
-        if(timeoutID)
+    $('#barcode-form').on('submit', function (e) {
+        e.preventDefault();
+        var barcode = input.val();
+        input.val("");
+        if (barcode.match(/^\d{10,13}$/))
         {
-            clearTimeout(timeoutID)
+            location.pathname = "/barcode/" + barcode;
         }
-        timeoutID = setTimeout(clearBuffer, 100)
-    }
-});
-
-function clearBuffer() {
-    keyBuffer = "";
-    timeoutID = null;
-}
+    })
+})
